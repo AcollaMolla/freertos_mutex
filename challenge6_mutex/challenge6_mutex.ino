f554239bb558a1d447e9fe6218726c5c220e0925
Anton <antengman@gmail.com>
2021-06-20 18:03:08 +0200

    Challenge code update

--- challenge6_mutex/challenge6_mutex.ino
+++ challenge6_mutex/challenge6_mutex.ino
@@ -1,54 +1,77 @@
+// Use only core 1 for demo purposes
 #if CONFIG_FREERTOS_UNICORE
-static const BaseType_t app_cpu = 0;
+  static const BaseType_t app_cpu = 0;
 #else
-static const BaseType_t app_cpu = 1;
+  static const BaseType_t app_cpu = 1;
 #endif
 
-static int shared_var = 0;
-static SemaphoreHandle_t mutex;
+// Pins (change this if your Arduino board does not have LED_BUILTIN defined)
+static const int led_pin = LED_BUILTIN;
 
-void incTask(void *parameters){
-  int local_var;
+//*****************************************************************************
+// Tasks
 
-  while(1){
-    if(xSemaphoreTake(mutex,0)==pdTRUE){
-      local_var = shared_var;
-      local_var++;
-      vTaskDelay(random(100,500)/portTICK_PERIOD_MS);
-      shared_var = local_var;
-      xSemaphoreGive(mutex);
-      Serial.println(shared_var); 
-    }
+// Blink LED based on rate passed by parameter
+void blinkLED(void *parameters) {
+
+  // Copy the parameter into a local variable
+  int num = *(int *)parameters;
+
+  // Print the parameter
+  Serial.print("Received: ");
+  Serial.println(num);
+
+  // Configure the LED pin
+  pinMode(led_pin, OUTPUT);
+
+  // Blink forever and ever
+  while (1) {
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(num / portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(num / portTICK_PERIOD_MS);
   }
 }
 
+//*****************************************************************************
+// Main (runs as its own task with priority 1 on core 1)
+
 void setup() {
-  randomSeed(analogRead(0));
+
+  long int delay_arg;
+
+  // Configure Serial
   Serial.begin(115200);
-  vTaskDelay(1000/portTICK_PERIOD_MS);
 
-  mutex = xSemaphoreCreateMutex();
+  // Wait a moment to start (so we don't miss Serial output)
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  Serial.println();
+  Serial.println("---FreeRTOS Mutex Challenge---");
+  Serial.println("Enter a number for delay (milliseconds)");
 
-  xTaskCreatePinnedToCore(incTask,
-    "Increment Task 1",
-    1024,
-    NULL,
-    1,
-    NULL,
-    app_cpu);
+  // Wait for input from Serial
+  while (Serial.available() <= 0);
 
-  xTaskCreatePinnedToCore(incTask,
-    "Increment Task 2",
-    1024,
-    NULL,
-    1,
-    NULL,
-    app_cpu);
+  // Read integer value
+  delay_arg = Serial.parseInt();
+  Serial.print("Sending: ");
+  Serial.println(delay_arg);
 
-    vTaskDelete(NULL);
+  // Start task 1
+  xTaskCreatePinnedToCore(blinkLED,
+                          "Blink LED",
+                          1024,
+                          (void *)&delay_arg,
+                          1,
+                          NULL,
+                          app_cpu);
+
+  // Show that we accomplished our task of passing the stack-based argument
+  Serial.println("Done!");
 }
 
-
 void loop() {
-
+  
+  // Do nothing but allow yielding to lower-priority tasks
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
 }
